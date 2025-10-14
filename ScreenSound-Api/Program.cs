e9f1b4cd6d53d2dbf9fbdb1ac7004cfa3b188db6
v-costajs5 <v-costajs5@BRK-BRJ3409TBP>
2025-10-14 20:44:06 -0300

    Add music details fetching and error handling
    This commit introduces new using directives for `ScreenSound_Api.Models` and `System.Text.Json`. It modifies the `HttpClient` usage to fetch a JSON response from a specified URL, deserialize it into a list of `Musica` objects, and display the details of the music at index 1998. Additionally, error handling is implemented to manage potential exceptions during the HTTP request and response processing.

--- ScreenSound-Api/Program.cs
+++ ScreenSound-Api/Program.cs
@@ -1,9 +1,14 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSound_Api.Models;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1998].ExibirDetalhesDaMusica();
     }
     catch (Exception ex)
     {
